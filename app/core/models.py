362ae3ae662ad4b076f084009dffc51f53e518fa
Tushar Khatiwada <khatiwadatushar@outlook.com>
2019-10-24 13:22:16 +0545

    Adds new isEmailVerified field on user model

--- app/core/models.py
+++ app/core/models.py
@@ -33,6 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
 
     objects = UserManager()
 
